579dd4ef8f734d13db2166d53635bc01758541de
cabbageDev <cbhong@ucsc.edu>
2021-01-27 23:56:40 -0800

    final, works

--- Assignment2/sort.c
+++ Assignment2/sort.c
@@ -5,7 +5,7 @@
 int* array;
 int split[5];
 int thread_num[5];
-pthread_t thread1, thread2, thread3, thread4;
+pthread_t threads[5];
 
 /* LEFT index and RIGHT index of the sub-array of ARR[] to be sorted */
 void singleThreadedMergeSort(int arr[], int left, int right) 
@@ -21,16 +21,16 @@ void singleThreadedMergeSort(int arr[], int left, int right)
 
 void* threadHelper(void* arg)
 {
-  int n = *((int*)arg);
-  if (n == 1){
-    singleThreadedMergeSort(array, split[n-1], split[n]);
-    pthread_join(thread2, NULL); //wait for t2 to sort
-    merge(array, split[n-1], split[n], split[n+1]); //Left side
+  int num = *((int *)arg);
+  if (num == 1){
+    singleThreadedMergeSort(array, split[n-1], split[n]);//Sorts LeftLeft
+    pthread_join(threads[2], NULL); //wait for t2 to sort
+    merge(array, split[num - 1], split[num], split[num + 1]); //Left side
   }
   singleThreadedMergeSort(array, split[n-1] + 1, split[n]);
-  if(n == 3){
-    pthread_join(thread4, NULL); //wait for t4 to sort 
-    merge(array, split[n-1] + 1, split[n], split[n+1]);//Right side 
+  if(num == 3){
+    pthread_join(threads[4], NULL); //wait for t4 to sort 
+    merge(array, split[num - 1] + 1, split[num], split[n + 1]);//Right side 
   }
   pthread_exit(NULL);
   return(NULL);
@@ -63,14 +63,14 @@ void multiThreadedMergeSort(int arr[], int left, int right)
   }
 
   
-  pthread_create(&thread1, NULL, &threadHelper, (void*)&thread_num[1]); //t1
-  pthread_create(&thread2, NULL, &threadHelper, (void*)&thread_num[2]); //t2
-  pthread_create(&thread3, NULL, &threadHelper, (void*)&thread_num[3]); //t3
-  pthread_create(&thread4, NULL, &threadHelper, (void*)&thread_num[4]); //t4
+  pthread_create(&threads[1], NULL, &threadHelper, (void*)&thread_num[1]); //t1
+  pthread_create(&threads[2], NULL, &threadHelper, (void*)&thread_num[2]); //t2
+  pthread_create(&threads[3], NULL, &threadHelper, (void*)&thread_num[3]); //t3
+  pthread_create(&threads[4], NULL, &threadHelper, (void*)&thread_num[4]); //t4
 
   //retrieve results of T1(left) & T3(right), then process merge
-  pthread_join(thread1, NULL);
-  pthread_join(thread3, NULL); 
+  pthread_join(threads[1], NULL);
+  pthread_join(threads[3], NULL); 
 
   merge(arr, left, split[2], right);
 
