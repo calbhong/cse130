de8741e6defb8f8b71738d705f57789ffaffd37b
Calvin Bin Hong <cbhong@thor.soe.ucsc.edu>
2021-01-27 22:30:29 -0800

    this bROKEN struct might just remove it

--- Assignment2/sort.c
+++ Assignment2/sort.c
@@ -1,6 +1,37 @@
 #include "merge.h"
 #include "stdio.h"
+#include "stdlib.h"
 #include "pthread.h"
+#include <string.h>
+
+int split[5];
+pthread_t threads[4];
+
+typedef struct data
+{
+  int* arr;
+  int thread_num;
+} data;
+
+void* threadSort(void* arg)
+{
+  data* ptr = (data*) arg;
+  int n = ptr->thread_num;
+  int* array = ptr->arr;
+
+  int x = 1;
+  if (n == 0){
+    x = 0;
+  }
+
+  singleThreadedMergeSort(array, split[n] + x ,split[n+1] );
+  if(n%2==0){
+    pthread_join(threads[x],NULL);
+    merge(array, split[n] + x, split[n+1], split[n+2]);
+  }
+  pthread_exit(NULL);
+  return(NULL);
+}
 
 /* LEFT index and RIGHT index of the sub-array of ARR[] to be sorted */
 void singleThreadedMergeSort(int arr[], int left, int right) 
@@ -13,18 +44,43 @@ void singleThreadedMergeSort(int arr[], int left, int right)
   } 
 }
 
+
 /* 
  * Multi Threaded Merge Sort
  * Helper Function Necessary for pthread_create()
  * Resrouces Used:
  * Lecture Secret Sauce
+ * Office Hours
  * 
  */
 void multiThreadedMergeSort(int arr[], int left, int right) 
 {
+  struct data thread_data[4];
+  //split array into 4 quarters
+  split[0] = left;
+  for(int i = 1; i<4; i++){
+    split[i] = i*(left+right)/4;
+    printf("%d", split[i]);
+  }
+  split[4] = right;
+
+
+
+  for(int i = 0; i < 4; i++){
+    &thread_data[i].arr = arr;
+    &thread_data.thread_num = i;
+  }
   
-  int i = 0;
   
+
+  pthread_create(&threads[0], NULL, &threadSort, &thread_data[0]);
+  pthread_create(&threads[1], NULL, &threadSort, &thread_data[1]);
+  pthread_create(&threads[2], NULL, &threadSort, &thread_data[2]);
+  pthread_create(&threads[3], NULL, &threadSort, &thread_data[3]);
+  pthread_join(threads[1], NULL);
+  pthread_join(threads[3], NULL); 
+
+  merge(arr, left, split[2], right);
 
   /* P splits array into 4 
   * P creates four threads to sort a 1/4 of the array each
